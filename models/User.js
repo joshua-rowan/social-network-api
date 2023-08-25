e9a67390948cf50339c1d01fea400a133d7419fc
CardsClaw <joshr1979@gmail.com>
2023-08-25 18:47:07 -0500

    initial commit

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/    
+        },
+        
+            thoughts: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Thought',
+                },
+            ],
+
+            friends: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'User'
+                }
+            ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+  const User = model('user', userSchema);
+
+  module.exports = User;
