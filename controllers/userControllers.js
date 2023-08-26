3d7d09337344d913058b5c3b1b749017f737f5a7
CardsClaw <joshr1979@gmail.com>
2023-08-26 11:08:49 -0500

    getSingleUser and createUser initial commit. getUsers and createUser work in Insomnia. troubleshooting getSingleUser

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -8,5 +8,28 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err);
         }
+    },
+
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userID });
+
+            if(!user) {
+                return res.status(404).json({ message: "No user with that ID" });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 };
