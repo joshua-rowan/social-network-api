084a345241858d0b24f9a6c6a96ccba384ca96f4
CardsClaw <joshr1979@gmail.com>
2023-08-26 11:42:20 -0500

    updateUser initial commit tested and working

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,13 +2,15 @@ const router = require('express').Router();
 const {
     getUsers,
     getSingleUser,
-    createUser
+    createUser,
+    updateUser
 } = require('../../controllers/userControllers.js');
 
 router.route('/').get(getUsers)
     .post(createUser);
 
 router.route('/:userId')
-    .get(getSingleUser);
+    .get(getSingleUser)
+    .put(updateUser);
 
 module.exports = router;
