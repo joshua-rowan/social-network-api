448f4772d6514a1259ac7aa0faa44fb068c2b220
CardsClaw <joshr1979@gmail.com>
2023-08-25 20:23:49 -0500

    initial Thought.js commit

---
+++ models/Thought.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+
+        username: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Reaction'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
