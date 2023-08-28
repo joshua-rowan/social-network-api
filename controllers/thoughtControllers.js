0fa5c4f9cf0e6cb47e3c1e38a443036876293653
CardsClaw <joshr1979@gmail.com>
2023-08-28 16:50:12 -0500

    getSingleThought and createThought initial commit

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -9,5 +9,37 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err);
         }
+    },
+
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+
+            if(!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+
+            const user = await User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $push: { thoughts: thought._id }},
+                { new: true }
+            );
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that Id'})
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 };
